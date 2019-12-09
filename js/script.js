cafc6c5783019bb6505158cfc0fc5c4e02e73e9b
JuanDuran85 <duran85juan@gmail.com>
2019-12-08 22:26:59 -0300

    cuadrado basico con moviemiento

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+//iniciando el canvas
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
+
+//iniciando variables del entorno
+let posicion = 0;
+let tamano = 0;
+
+setInterval(function () {
+	context.clearRect(0,0,400,400);
+	context.fillRect(posicion,0,tamano,tamano);
+
+	posicion++;
+	tamano++;
+
+	if (posicion>400) {
+		posicion = 0;
+		tamano = 0
+		context.fillStyle = "Red";
+	};
+}, 30);
